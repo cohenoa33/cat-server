4735ade4a38aa5227d40244367343f36ef7fc9c9
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-03-14 18:14:23 -0700

    validations to user

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+const { registerValidation } = require("./validation");
+
 router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exist");
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -9,7 +17,7 @@ router.post("/register", async (req, res) => {
   });
 
   try {
-    console.log("works");
+    console.log("User saved to DB");
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
